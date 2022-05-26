48c51d907ac2e5f14fed902f7f601ad4181cb3df
Felipe Holanda de Freitas <79938668+felipeholanda2077@users.noreply.github.com>
2022-05-26 15:23:12 -0300

    Em Andamento

--- App.js
+++ App.js
@@ -6,45 +6,45 @@ class App extends Component {
   render() {
 
     //Imagens Links
-    let Banner = "https://www.agencianectarina.com.br/lojavirtual/wp-content/uploads/2021/04/banner-home-2.jpg";
-    let Roupas_F = require ('../LojadeRoupas/img/Mulher1.jpeg')
-    let Roupas_F2 = require ('../LojadeRoupas/img/Mulher2.jpeg')
-    let Roupas_F3 = "https://img.lojasrenner.com.br/item/631863443/zoom/1.jpg";
-    let Roupas_M = "https://shopee.com.br/blog/wp-content/uploads/2022/03/shoppe0150-1.jpg";
-    let Roupas_M2 = "https://mhmcdn.manualdohomemmoderno.com.br/?w=750&h=450&quality=31&clipping=landscape&url=https://manualdohomemmoderno.com.br/files/2019/06/onde-comprar-roupa-masculina-barata-as-melhores-lojas-da-internet-onde-comprar-roupa-masculina-barata-as-melhores-lojas-da-internet-4.jpg";
-    let Roupas_M3 = "https://img.lojasrenner.com.br/item/551909376/zoom/1.jpg"
-    let Roupas_M4 = "https://img.lojasrenner.com.br/item/601206258/zoom/1.jpg"
-    
+    let Banner1 = require ('../LojadeRoupas/img/Banner.jpeg');
+    let Roupas_F = require ('../LojadeRoupas/img/Mulher1.jpeg');
+    let Roupas_F1 = require ('../LojadeRoupas/img/Mulher2.jpeg');
+    let Roupas_F2 = require ('../LojadeRoupas/img/Mulher3.jpeg');
+    let Roupas_M = require ('../LojadeRoupas/img/Homem1.jpeg');
+    let Roupas_M2 = require ('../LojadeRoupas/img/Homem2.jpeg');
+    let Roupas_M3 = require ('../LojadeRoupas/img/Homem3.jpeg');
+    let Roupas_M4 = require ('../LojadeRoupas/img/Homem4.jpeg');
+
     //Carossel Banner
     let imagens = []
-    for (let i = 0; i < 1; i++) { 
-      imagens.push(<Image style={{alignItems: 'center', width: 580, height: 270, margin: 10, borderRadius: 10 }} source={{ uri: Banner }} />)
+    for (let i = 0; i < 1; i++) {
+      imagens.push(<Image style={{ alignItems: 'center', width: 580, height: 270, margin: 10, borderRadius: 10 }} source={{ uri: Banner1 }} />)
     }
     //Carrosel Feminino
     let Carrosel = []
     for (let i = 0; i < 1; i++) {
-      Carrosel.push(<Image style={{ width: 200, height: 350, margin: 10, borderRadius: 5 }} source={{ uri: Roupas_F }} />)
+      Carrosel.push(<Image style={{margin: 10, borderRadius: 5 }} source={{ uri: Roupas_F }} />)
     }
     for (let i = 0; i < 1; i++) {
-      Carrosel.push(<Image style={{ width: 200, height: 350, margin: 10, borderRadius: 5 }} source={{ uri: Roupas_F2 }} />)
+      Carrosel.push(<Image style={{margin: 10, borderRadius: 5 }} source={{ uri: Roupas_F1 }} />)
     }
     for (let i = 0; i < 1; i++) {
-      Carrosel.push(<Image style={{ width: 200, height: 350, margin: 10, borderRadius: 5 }} source={{ uri: Roupas_F3 }} />)
+      Carrosel.push(<Image style={{margin: 10, borderRadius: 5 }} source={{ uri: Roupas_F2 }} />)
     }
-    
+
     //Carrosel Masculino
     let Masculino = []
     for (let i = 0; i < 1; i++) {
-      Masculino.push(<Image style={{ width: 200, height: 350, margin: 10, borderRadius: 5 }} source={{ uri: Roupas_M }} />)
+      Masculino.push(<Image style={{margin: 10, borderRadius: 5 }} source={{ uri: Roupas_M }} />)
     }
     for (let i = 0; i < 1; i++) {
-      Masculino.push(<Image style={{ width: 200, height: 350, margin: 10, borderRadius: 5 }} source={{ uri: Roupas_M2 }} />)
+      Masculino.push(<Image style={{margin: 10, borderRadius: 5 }} source={{ uri: Roupas_M2 }} />)
     }
     for (let i = 0; i < 1; i++) {
-      Masculino.push(<Image style={{ width: 200, height: 350, margin: 10, borderRadius: 5 }} source={{ uri: Roupas_M3 }} />)
+      Masculino.push(<Image style={{margin: 10, borderRadius: 5 }} source={{ uri: Roupas_M3 }} />)
     }
     for (let i = 0; i < 1; i++) {
-      Masculino.push(<Image style={{ width: 200, height: 350, margin: 10, borderRadius: 5 }} source={{ uri: Roupas_M4 }} />)
+      Masculino.push(<Image style={{margin: 10, borderRadius: 5 }} source={{ uri: Roupas_M4 }} />)
     }
 
 
@@ -61,16 +61,20 @@ class App extends Component {
           <Text style={{ color: 'black', fontSize: 20, textAlign: 'center', margin: 7, paddingTop: 20 }}>Moda Feminina</Text>
           <Text style={{ color: 'black', fontSize: 10, textTransform: 'uppercase', textAlign: 'left', paddingTop: 10 }}></Text>
           <ScrollView horizontal={true}>
+
             {/* Chamada do Array que ira inserir as imagens vezes */}
             {Carrosel}
           </ScrollView>
 
+
           <Text style={{ color: 'black', fontSize: 20, textAlign: 'center', paddingTop: 40 }}>Moda Masculina</Text>
           <Text style={{ color: 'black', fontSize: 10, textTransform: 'uppercase', textAlign: 'left', paddingTop: 10 }}></Text>
           <ScrollView horizontal={true}>
+
             {/* Chamada do Array que ira inserir as imagens vezes */}
             {Masculino}
           </ScrollView>
+
 
           <Text style={{ color: 'black', fontSize: 20, textAlign: 'left', margin: 50, paddingTop: 40 }}>Bem Vindo ao BAZAAR</Text>
           <Text style={{ color: 'black', fontSize: 16, textAlign: 'left', margin: 50 }}>Nossos produtos são inspirados pelas pessoas que esão á nossa volta, com suas belezas e qualidades.\n Produtos de bom gosto especialmente para você.\n Descubra "our story" e aproveite as promoçôes.</Text>
