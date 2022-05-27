1519c6d3d3fe778550caca0eed1f52832c9b9c2a
Felipe Holanda de Freitas <79938668+felipeholanda2077@users.noreply.github.com>
2022-05-27 14:36:14 -0300

    Em Andamento

--- App.js
+++ App.js
@@ -1,56 +1,56 @@
-// Importamos as bibliotecasecomponentes principais para nosso projeto
 import React, { Component } from 'react';
 import { View, Text, Button, Image, ScrollView } from 'react-native';
-// Criamosanossa classe App para renderizar nosso projeto
+import { form } from '../LojadeRoupas/index'
+
 class App extends Component {
   render() {
 
     //Imagens Links
-    let Banner1 = require ('../LojadeRoupas/img/Banner.jpeg');
-    let Roupas_F = require ('../LojadeRoupas/img/Mulher1.jpeg');
-    let Roupas_F1 = require ('../LojadeRoupas/img/Mulher2.jpeg');
-    let Roupas_F2 = require ('../LojadeRoupas/img/Mulher3.jpeg');
-    let Roupas_M = require ('../LojadeRoupas/img/Homem1.jpeg');
-    let Roupas_M2 = require ('../LojadeRoupas/img/Homem2.jpeg');
-    let Roupas_M3 = require ('../LojadeRoupas/img/Homem3.jpeg');
-    let Roupas_M4 = require ('../LojadeRoupas/img/Homem4.jpeg');
+    let Banner1 = "https://siberian.vteximg.com.br/arquivos/ids/177011/NOVIDADES%20MASCULINO%20MOBILE.jpg?v=637758892402070000";
+    let Roupas_F = "https://img.lojasrenner.com.br/item/624063528/zoom/1.jpg"
+    let Roupas_F1 = "https://img.lojasrenner.com.br/item/624030110/zoom/1.jpg";
+    let Roupas_F2 = "https://img.lojasrenner.com.br/item/598798600/zoom/1.jpg";
+    let Roupas_M = "https://damyller.vteximg.com.br/arquivos/ids/597598-475-713/Jaqueta-Biker-Resinada-Masculina-Frente--.jpg?v=637497653752370000";
+    let Roupas_M2 = "https://ph-cdn1.ecosweb.com.br/imagens01/foto/moda-masculina/camisetas-masculinas/camiseta-manga-longa-gola-role-preta_23170_301_1.jpg";
+    let Roupas_M3 = "https://ph-cdn1.ecosweb.com.br/imagens01/foto/moda-masculina/camiseta-manga-curta/camiseta-at-roger-marinho_307888_301_1.jpg";
+    let Roupas_M4 = "https://img.lojasrenner.com.br/item/597578701/zoom/1.jpg";
 
     //Carossel Banner
     let imagens = []
     for (let i = 0; i < 1; i++) {
-      imagens.push(<Image style={{ alignItems: 'center', width: 580, height: 270, margin: 10, borderRadius: 10 }} source={{ uri: Banner1 }} />)
+      imagens.push(<Image style={{ alignItems: 'center', width: 540, height: 400, margin: 10, borderRadius: 10 }} source={{ uri: Banner1}} />)
     }
     //Carrosel Feminino
     let Carrosel = []
     for (let i = 0; i < 1; i++) {
-      Carrosel.push(<Image style={{margin: 10, borderRadius: 5 }} source={{ uri: Roupas_F }} />)
+      Carrosel.push(<Image style={{width: 200, height: 350, margin: 10, borderRadius: 5 }} source={{ uri: Roupas_F}} />)
     }
     for (let i = 0; i < 1; i++) {
-      Carrosel.push(<Image style={{margin: 10, borderRadius: 5 }} source={{ uri: Roupas_F1 }} />)
+      Carrosel.push(<Image style={{width: 200, height: 350, margin: 10, borderRadius: 5 }} source={{ uri: Roupas_F1}} />)
     }
     for (let i = 0; i < 1; i++) {
-      Carrosel.push(<Image style={{margin: 10, borderRadius: 5 }} source={{ uri: Roupas_F2 }} />)
+      Carrosel.push(<Image style={{width: 200, height: 350, margin: 10, borderRadius: 5 }} source={{ uri: Roupas_F2}} />)
     }
 
     //Carrosel Masculino
     let Masculino = []
     for (let i = 0; i < 1; i++) {
-      Masculino.push(<Image style={{margin: 10, borderRadius: 5 }} source={{ uri: Roupas_M }} />)
+      Masculino.push(<Image style={{width: 200, height: 350,margin: 10, borderRadius: 5 }} source={{ uri: Roupas_M }} />)
     }
     for (let i = 0; i < 1; i++) {
-      Masculino.push(<Image style={{margin: 10, borderRadius: 5 }} source={{ uri: Roupas_M2 }} />)
+      Masculino.push(<Image style={{width: 200, height: 350, margin: 10, borderRadius: 5 }} source={{ uri: Roupas_M2 }} />)
     }
     for (let i = 0; i < 1; i++) {
-      Masculino.push(<Image style={{margin: 10, borderRadius: 5 }} source={{ uri: Roupas_M3 }} />)
+      Masculino.push(<Image style={{width: 200, height: 350, margin: 10, borderRadius: 5 }} source={{ uri: Roupas_M3 }} />)
     }
     for (let i = 0; i < 1; i++) {
-      Masculino.push(<Image style={{margin: 10, borderRadius: 5 }} source={{ uri: Roupas_M4 }} />)
+      Masculino.push(<Image style={{width: 200, height: 350, margin: 10, borderRadius: 5 }} source={{ uri: Roupas_M4 }} />)
     }
 
 
     return (
       <ScrollView>
-        <View>
+        <View style={form.container}>
           <Text style={{ color: 'black', fontSize: 20, textTransform: 'uppercase', margin: 7, paddingTop: 20 }}>BAZAAR</Text>
           <Text style={{ color: 'black', fontSize: 10, textTransform: 'uppercase', textAlign: 'left', paddingTop: 10 }}></Text>
 
@@ -75,9 +75,18 @@ class App extends Component {
             {Masculino}
           </ScrollView>
 
+          <Text style={{ color: 'black', fontSize: 16, textAlign: 'left', margin: 40 }}>Bem Vindo ao BAZAAR{'\n'}{'\n'}{'\n'}Nossos produtos são inspirados pelas pessoas que estão á nossa volta, com suas belezas e qualidades.{'\n'}{'\n'}Produtos de bom gosto especialmente para você.{'\n'}Descubra "our story" e aproveite as promoçôes imperdiveis.</Text>
+          <Text style={{ color: 'black', fontSize: 20, textAlign: 'center', margin: 50, paddingTop: 10 }}>----------------------------------------------------------------------------</Text>
+          <Text style={{ color: 'black', fontSize: 20, textAlign: 'center', margin: 50, paddingTop: 10 }}>Nosso endereço{'\n'}{'\n'}Rua Eusébio 10{'\n'}Centro, Cidade das Águas{'\n'}{'\n'}Abertura de segunda a sexta das 10:00 ás 21:00{'\n'}{'\n'}----------------------------------------------------------------------------</Text>
 
-          <Text style={{ color: 'black', fontSize: 20, textAlign: 'left', margin: 50, paddingTop: 40 }}>Bem Vindo ao BAZAAR</Text>
-          <Text style={{ color: 'black', fontSize: 16, textAlign: 'left', margin: 50 }}>Nossos produtos são inspirados pelas pessoas que esão á nossa volta, com suas belezas e qualidades.\n Produtos de bom gosto especialmente para você.\n Descubra "our story" e aproveite as promoçôes.</Text>
+          <Text style={{ color: 'black', fontSize: 20, textAlign: 'left', margin: 50, paddingTop: 10 }}>Siga-nos</Text>
+
+
+          <Text style={{ color: 'black', fontSize: 20, textAlign: 'left', margin: 50, paddingTop: 10 }}>Aceitamos</Text>
+
+
+          <Text style={{ color: 'black', fontSize: 13, textAlign: 'center', paddingTop: 50 }}>© Felipe Holanda - BAZAAR 2022</Text>
+
 
         </View>
       </ScrollView>
